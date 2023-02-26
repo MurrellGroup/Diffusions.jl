abb0335047fcab25744542377c05cc5fa2b4b22d
Kenta Sato <bicycle1885@gmail.com>
2023-02-26 22:54:42 +0100

    parameterize GaussianVariables with variance

--- src/randomvariable.jl
+++ src/randomvariable.jl
@@ -2,19 +2,19 @@
 # ----------------
 
 struct GaussianVariables{T, A <: AbstractArray{T}}
-    # μ and σ must have the same size
-    μ::A
-    σ::A
+    # μ and σ² must have the same size
+    μ::A   # mean
+    σ²::A  # variance
 end
 
 Base.size(X::GaussianVariables) = size(X.μ)
 
-sample(rng::AbstractRNG, X::GaussianVariables{T}) where T = randn(rng, T, size(X)) .* X.σ .+ X.μ
+sample(rng::AbstractRNG, X::GaussianVariables{T}) where T = randn(rng, T, size(X)) .* .√X.σ² .+ X.μ
 
 function combine(X::GaussianVariables, lik)
-    σ² = @. inv(inv(X.σ^2) + inv(lik.σ^2))
-    μ = @. σ² * (X.μ / X.σ^2 + lik.μ / lik.σ^2)
-    return GaussianVariables(μ, sqrt.(σ²))
+    σ² = @. inv(inv(X.σ²) + inv(lik.σ²))
+    μ = @. σ² * (X.μ / X.σ² + lik.μ / lik.σ²)
+    return GaussianVariables(μ, σ²)
 end
 
 
