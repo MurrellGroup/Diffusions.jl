1a0263902396897180e3a3c80653e747a45a3d2c
Kenta Sato <bicycle1885@gmail.com>
2023-02-27 17:33:46 +0100

    make rotation diffusions consistent (fix #12)

--- src/rotational.jl
+++ src/rotational.jl
@@ -3,43 +3,47 @@ export
     MultiRotationState,
     features
 
-function randrot(rng, σsq)
-    σ = sqrt(σsq)
-    QuatRotation(exp(quat(0, randn(rng) * σ, randn(rng) * σ, randn(rng) * σ)))
+function randrot(rng::AbstractRNG, σ²::Real)
+    σ = √σ²
+    return QuatRotation(exp(quat(0, randn(rng) * σ, randn(rng) * σ, randn(rng) * σ)))
 end
 
 Quaternions.slerp(qa::QuatRotation{Float64},qb::QuatRotation{Float64},t) = QuatRotation(slerp(qa.q,qb.q,t))
 
 #T is in units of var
-function rotation_diffuse(rng, Rstart, T; max_var_step = 0.05)
+function rotation_diffuse(rng::AbstractRNG, Rstart::QuatRotation, T::Real; max_var_step::Real = 0.05)
     remaining_var = T
-    B = copy(Rstart)
+    B = Rstart
     for t in max_var_step:max_var_step:T
-        B = B * randrot(rng, max_var_step)
-        remaining_var = T-t
+        B *= randrot(rng, max_var_step)
+        remaining_var = T - t
     end
-    B = B * randrot(rng, remaining_var)
+    B *= randrot(rng, remaining_var)
     return B
 end
 
 #T is in units of var
-function rotation_bridge(rng, Rstart, Rend, eps, T; max_var_step = 0.05)
-    B = rotation_diffuse(rng, Rstart, T - eps, max_var_step = max_var_step)
-    C = rotation_diffuse(rng, B, eps, max_var_step = max_var_step)
-    difference_rot = slerp(C, Rend, (T-eps)/T)
+function rotation_bridge(
+    rng::AbstractRNG,
+    Rstart::QuatRotation,
+    Rend::QuatRotation,
+    eps::Real,
+    T::Real;
+    max_var_step::Real = 0.05
+)
+    B = rotation_diffuse(rng, Rstart, T - eps; max_var_step)
+    C = rotation_diffuse(rng, B, eps; max_var_step)
+    difference_rot = slerp(C, Rend, (T - eps) / T)
     return B * C' * difference_rot
 end
 
 
-mutable struct RotDiffusionProcess <: SimulationProcess
-    rate::Float64
-    function RotDiffusionProcess(rate)
-        new(rate)
-    end
-    function RotDiffusionProcess()
-        new(1.0)
-    end
+struct RotDiffusionProcess{T <: Real} <: SimulationProcess
+    rate::T
 end
+
+RotDiffusionProcess() = RotDiffusionProcess(1.0)
+
 
 mutable struct MultiRotationState <: ContinuousState
     rots::Array{QuatRotation}
@@ -54,21 +58,11 @@ function forward_sample!(end_state,init_state,P::RotDiffusionProcess,T)
     end
 end
 
-function sampleforward(rng::AbstractRNG, process::RotDiffusionProcess, t::Real, x)
-    x_t = MultiRotationState(size(x.rots)...)
-    for i in eachindex(x.rots)
-        x_t.rots[i] = rotation_diffuse(rng, x.rots[i], t * process.rate)
-    end
-    return x_t
-end
+sampleforward(rng::AbstractRNG, process::RotDiffusionProcess, t::Real, x) =
+    rotation_diffuse.(rng, x, t * process.rate)
 
-function endpoint_conditioned_sample(rng::AbstractRNG, process::RotDiffusionProcess, s::Real, t::Real, x_0, x_t)
-    x_s = MultiRotationState(size(x_0.rots)...)
-    for i in eachindex(x_0.rots)
-        x_s.rots[i] = rotation_bridge(rng, x_0.rots[i], x_t.rots[i], (t - s) * process.rate, t * process.rate)
-    end
-    return x_s
-end
+endpoint_conditioned_sample(rng::AbstractRNG, process::RotDiffusionProcess, s::Real, t::Real, x_0, x_t) =
+    rotation_bridge.(rng, x_0, x_t, (t - s) * process.rate, t * process.rate)
 
 values(r::MultiRotationState) = copy(r.rots)
 
