a2db103ee5a50f49d30529382db6ebf5872561a8
Kenta Sato <bicycle1885@gmail.com>
2023-03-05 15:12:15 +0100

    delete legacy code (#19)

--- src/continuous.jl
+++ src/continuous.jl
@@ -11,77 +11,18 @@ var(model::OrnsteinUhlenbeck) = (model.volatility^2) / (2 * model.reversion)
 
 eq_dist(model::OrnsteinUhlenbeck) = Normal(model.mean,var(model))
 
-#MultiGaussianstate
-#Geared for DL, this is an arbitrarily shaped Gaussian state. Aim is performant diffusion.
-#No normalizing constants!
-#Should probably see if MultiGaussianState{T,N} adds speed.
-mutable struct MultiGaussianState <: ContinuousState
-    mean::Array
-    var::Array
-end
-
-function combine!(dest::MultiGaussianState, src::MultiGaussianState)
-    newvar = 1 ./ (1 ./ dest.var .+ 1 ./ src.var)
-    dest.mean .= newvar .* (dest.mean ./ dest.var .+ src.mean ./ src.var)
-    dest.var = newvar
-end
-
-#Mistake: this dest var ignores the source var.
-#We're effectively treating all sources as point masses, which is fine because we only need this for endpoint-conditioned sampling
-#But I need to fix the OU process in MolEv!
-function forward!(
-    dest::MultiGaussianState,
-    source::MultiGaussianState,
-    process::OrnsteinUhlenbeck,
-    t::Real,
-)
-    t = oftype(process.mean, t)
-    #if source.var > 0.000001
-    #    @error "OU forward diffusion only currently implemented from a zero-variance point mass"
-    #end
-    dest.mean .=
-        (exp(-t * process.reversion)) .* (source.mean .- process.mean) .+ process.mean
-    dest.var .=
-        ((1 - exp(-2 * t * process.reversion)) * process.volatility^2) /
-        (2 * process.reversion)
-end
-
-function backward!(
-    dest::MultiGaussianState,
-    source::MultiGaussianState,
-    process::OrnsteinUhlenbeck,
-    t::Real,
-)
-    t = oftype(process.mean, t)
-    #if source.var > 0.000001
-    #    @error "OU backward diffusion only currently implemented from a zero-variance point mass"
-    #end
-    theta = process.reversion
-    sigma = process.volatility
-    dest.mean .=
-        (exp(t * theta)) .* (source.mean .- process.mean) .+ process.mean
-    dest.var .=
-        -((sigma^2)/(2*theta)) + (sigma^2 * exp(2*t*theta))/(2*theta)
-end
-
-function sample!(g::MultiGaussianState)
-    g.mean .= randn(size(g.mean)) .* sqrt.(g.var) .+ g.mean
-    g.var .= 0.0;
-end
-
-#This is what gets used for tracking the reverse sampling
-values(g::MultiGaussianState) = copy(g.mean)
-
 function forward(process::OrnsteinUhlenbeck, x_s::AbstractArray, s::Real, t::Real)
-    dest = MultiGaussianState(similar(x_s), similar(x_s))
-    source = MultiGaussianState(x_s, [])  # the second field is not used
-    forward!(dest, source, process, t - s)
-    return GaussianVariables(dest.mean, dest.var)
+    μ, σ, θ = process.mean, process.volatility, process.reversion
+    mean = @. exp(-(t - s) * θ) * (x_s - μ) + μ
+    var = similar(mean)
+    var .= ((1 - exp(-2(t - s) * θ)) * σ^2) / 2θ
+    return GaussianVariables(mean, var)
 end
 
 function backward(process::OrnsteinUhlenbeck, x_t::AbstractArray, s::Real, t::Real)
-    dest = MultiGaussianState(similar(x_t), similar(x_t))
-    source = MultiGaussianState(x_t, [])  # the second field is not used
-    backward!(dest, source, process, t - s)
-    return (μ = dest.mean, σ² = dest.var)
+    μ, σ, θ = process.mean, process.volatility, process.reversion
+    mean = @. exp((t - s) * θ) * (x_t - μ) + μ
+    var = similar(mean)
+    var .= -(σ^2 / 2θ) + (σ^2 * exp(2(t - s) * θ)) / 2θ
+    return (μ = mean, σ² = var)
 end
--- src/discrete.jl
+++ src/discrete.jl
@@ -1,47 +1,3 @@
-#Discrete process
-
-#These work for the generic discrete type
-function combine!(dest::DiscreteState, src::DiscreteState)
-    # Update the state
-    dest.state .*= src.state
-    @views for j in axes(dest.state, 2)
-        dest.state[:,j] ./= sum(dest.state[:,j])
-    end
-end
-
-function one_hot_sample(vec::Vector{<:Real})
-    vec .= vec ./ sum(vec)
-    ind = rand(Categorical(vec))#sample(1:length(vec), Weights(vec))
-    sampled = zeros(eltype(vec), size(vec))
-    sampled[ind] = 1.0
-    return sampled
-end
-
-#Replace each column with a 1-of-k vector that is a categorical draw from that row.
-function sample!(partition::DiscreteState)
-    for i = 1:partition.sites
-        partition.state[:, i] .= one_hot_sample(partition.state[:, i])
-    end
-end
-
-#I need to make a version of this with more dimensional flexibility
-#This is not just a DiscreteState, because maybe there could be some <: DiscreteState with specialized ops, like AAs
-mutable struct MultiDiscreteState <: DiscreteState
-    state::Array{Float64,2}
-    states::Int
-    sites::Int
-    function MultiDiscreteState(states, sites)
-        new(zeros(states, sites), states, sites)
-    end
-    function MultiDiscreteState(freq_vec::Vector{Float64}, sites::Int64)
-        state_arr = zeros(length(freq_vec), sites)
-        state_arr .= freq_vec
-        new(state_arr, length(freq_vec), sites)
-    end
-end
-
-values(g::MultiDiscreteState) = argmax.(eachcol(g.state))
-
 #This is basically F81, but with arb state size and linear prop ops
 #IJ = "Independent Jumps", as in every time a mutation event happens, you jump to a new state independent of the current state.
 struct IJ{T <: Real} <: DiscreteStateProcess
@@ -55,6 +11,8 @@ struct IJ{T <: Real} <: DiscreteStateProcess
         return new{T}(r, π, β)
     end
 end
+
+eq_dist(model::IJ) = Categorical(model.pi)
 
 function forward(process::IJ, x_s::AbstractArray, s::Real, t::Real)
     (;r, π, β) = process
@@ -70,5 +28,3 @@ function backward(process::IJ, x_t::AbstractArray, s::Real, t::Real)
     c1 = (1 - pow) .* π
     return pow .* x_t .+ sum(x_t .* c1, dims = 1)
 end
-
-eq_dist(model::IJ) = Categorical(model.pi)
