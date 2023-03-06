e1d8cfa562d49a7491a540e971f0af544c4ceeca
Kenta Sato <bicycle1885@gmail.com>
2023-03-06 21:28:20 +0100

    add MNIST example (#6)

---
+++ examples/mnist/model.jl
@@ -0,0 +1,121 @@
+# The code in this file is derived from the diffusion_mnist example at model-zoo.
+# https://github.com/FluxML/model-zoo/tree/master/vision/diffusion_mnist
+# The license is MIT: https://github.com/FluxML/model-zoo/blob/master/LICENSE.md
+
+using Flux
+using Flux: @functor
+using MLUtils: flatten
+
+"""
+Projection of Gaussian Noise onto a time vector.
+# Notes
+This layer will help embed our random times onto the frequency domain. \n
+W is not trainable and is sampled once upon construction - see assertions below.
+# References
+paper-  https://arxiv.org/abs/2006.10739
+"""
+function GaussianFourierProjection(embed_dim, scale)
+    # Instantiate W once
+    W = randn(Float32, embed_dim ÷ 2) .* scale
+    # Return a function that always references the same W
+    function (t)
+        t_proj = t' .* W * Float32(2π)
+        return [sin.(t_proj); cos.(t_proj)]
+    end
+end
+
+"""
+Create a UNet architecture as a backbone to a diffusion model. \n
+# Notes
+Images stored in WHCN (width, height, channels, batch) order. \n
+In our case, MNIST comes in as (28, 28, 1, batch). \n
+# References
+paper-  https://arxiv.org/abs/1505.04597
+"""
+struct UNet
+    layers::NamedTuple
+end
+
+@functor UNet
+
+"""
+User Facing API for UNet architecture.
+"""
+function UNet(c, channels=[32, 64, 128, 256], embed_dim=256, scale=30.0f0)
+    k = 10  # number of classes
+    return UNet((
+        gaussfourierproj=GaussianFourierProjection(embed_dim, scale),
+        labelproj=Dense(k, embed_dim),
+        linear=Dense(embed_dim, embed_dim, swish),
+        # Encoding
+        conv1=Conv((3, 3), c => channels[1], stride=1, pad=1, bias=false),
+        dense1=Dense(embed_dim, channels[1]),
+        gnorm1=GroupNorm(channels[1], 4, swish),
+        conv2=Conv((3, 3), channels[1] => channels[2], stride=2, pad=1, bias=false),
+        dense2=Dense(embed_dim, channels[2]),
+        gnorm2=GroupNorm(channels[2], 32, swish),
+        conv3=Conv((3, 3), channels[2] => channels[3], stride=2, pad=1, bias=false),
+        dense3=Dense(embed_dim, channels[3]),
+        gnorm3=GroupNorm(channels[3], 32, swish),
+        conv4=Conv((3, 3), channels[3] => channels[4], stride=2, pad=1, bias=false),
+        dense4=Dense(embed_dim, channels[4]),
+        gnorm4=GroupNorm(channels[4], 32, swish),
+        # Decoding
+        tconv4=ConvTranspose((3, 3), channels[4] => channels[3], stride=2, pad=1, bias=false),
+        dense5=Dense(embed_dim, channels[3]),
+        tgnorm4=GroupNorm(channels[3], 32, swish),
+        tconv3=ConvTranspose((3, 3), channels[3] + channels[3] => channels[2], pad=(1, 0, 1, 0), stride=2, bias=false),
+        dense6=Dense(embed_dim, channels[2]),
+        tgnorm3=GroupNorm(channels[2], 32, swish),
+        tconv2=ConvTranspose((3, 3), channels[2] + channels[2] => channels[1], pad=(1, 0, 1, 0), stride=2, bias=false),
+        dense7=Dense(embed_dim, channels[1]),
+        tgnorm2=GroupNorm(channels[1], 32, swish),
+        tconv1=ConvTranspose((3, 3), channels[1] + channels[1] => 1, stride=1, pad=1, bias=false),
+        # Classification
+        maxpool=GlobalMaxPool(),
+        class=Dense(channels[4] => k, bias=false),
+    ))
+end
+
+"""
+Makes the UNet struct callable and shows an example of a "Functional" API for modeling in Flux. \n
+"""
+function (unet::UNet)(x, y, t)
+    # Embedding
+    embed = unet.layers.linear(unet.layers.gaussfourierproj(t) .+ unet.layers.labelproj(y))
+    # Encoder
+    h1 = unet.layers.conv1(x)
+    h1 = h1 .+ expand_dims(unet.layers.dense1(embed), 2)
+    h1 = unet.layers.gnorm1(h1)
+    h2 = unet.layers.conv2(h1)
+    h2 = h2 .+ expand_dims(unet.layers.dense2(embed), 2)
+    h2 = unet.layers.gnorm2(h2)
+    h3 = unet.layers.conv3(h2)
+    h3 = h3 .+ expand_dims(unet.layers.dense3(embed), 2)
+    h3 = unet.layers.gnorm3(h3)
+    h4 = unet.layers.conv4(h3)
+    h4 = h4 .+ expand_dims(unet.layers.dense4(embed), 2)
+    h4 = unet.layers.gnorm4(h4)
+    # Decoder
+    h = unet.layers.tconv4(h4)
+    h = h .+ expand_dims(unet.layers.dense5(embed), 2)
+    h = unet.layers.tgnorm4(h)
+    h = unet.layers.tconv3(cat(h, h3; dims=3))
+    h = h .+ expand_dims(unet.layers.dense6(embed), 2)
+    h = unet.layers.tgnorm3(h)
+    h = unet.layers.tconv2(cat(h, h2; dims=3))
+    h = h .+ expand_dims(unet.layers.dense7(embed), 2)
+    h = unet.layers.tgnorm2(h)
+    h = unet.layers.tconv1(cat(h, h1; dims=3))
+    # Classification
+    logits = unet.layers.class(flatten(unet.layers.maxpool(h4)))
+    return h, logits
+end
+
+"""
+Helper function that adds `dims` dimensions to the front of a `AbstractVecOrMat`.
+Similar in spirit to TensorFlow's `expand_dims` function.
+# References:
+https://www.tensorflow.org/api_docs/python/tf/expand_dims
+"""
+expand_dims(x::AbstractVecOrMat, dims::Int=2) = reshape(x, (ntuple(i -> 1, dims)..., size(x)...))
---
+++ examples/mnist/train.jl
@@ -0,0 +1,122 @@
+using BSON: @save
+using Dates: Dates, now
+using Diffusions
+using Distributions: Uniform
+using Flux.Data: DataLoader
+using Flux.Losses: mse, logitcrossentropy
+using MLDatasets: MNIST, FashionMNIST
+using OneHotArrays: onehotbatch
+using Optimisers: Optimisers, WeightDecay, Adam
+using Printf: @printf, @sprintf
+using Statistics: mean
+
+include("model.jl")
+
+n_epochs = 500
+batchsize = 512
+
+# relative weight of the classification loss relative to the reconstruction loss
+λ = 0.01f0
+
+# self-conditioned training
+self_conditioned = true
+
+# Data loading
+if isempty(ARGS) || lowercase(ARGS[1]) == "mnist"
+    dataset = MNIST
+elseif lowercase(ARGS[1]) == "fashionmnist"
+    dataset = FashionMNIST
+else
+    error("the dataset name must be MNIST or FashionMNIST")
+end
+preprocess((x, y)) = reshape(x, 28, 28, 1, :), onehotbatch(y, 0:9)
+xtrain, ytrain = preprocess(dataset(:train)[:])
+xtest, ytest = preprocess(dataset(:test)[:])
+dataloader_train = DataLoader((xtrain, ytrain); batchsize)
+dataloader_test = DataLoader((xtest, ytest); batchsize)
+
+# Model and optimizer setup
+model = UNet(1 + self_conditioned)
+optim = Adam(1f-3)
+state = Optimisers.setup(optim, model)
+
+device = gpu
+model = device(model)
+state = device(state)
+
+# Diffusion scheduling
+p = ones(Float32, 10) ./ 10  # equilibrium distribution
+θ = 4.0f0  # reversion
+σ = √(2θ)  # volatility
+diffusion = (OrnsteinUhlenbeckDiffusion(0, σ, θ), IndependentDiscreteDiffusion(0.5f0, p))
+
+t_min = 1f-4
+t_max = 1f+1
+sampletime() = Float32(exp(rand(Uniform(log(t_min), log(t_max)))))
+
+# Diffuse a batch of images and labels up to random times
+function diffuse(x, y)
+    n = size(x, 4)
+    diffused = (x = similar(x), y = similar(y), t = zeros(Float32, n))
+    for i in 1:n
+        time = sampletime()
+        diffused.t[i] = time
+        diffused.x[:,:,:,i], diffused.y[:,i] =
+            sampleforward(diffusion, time, (x[:,:,:,i], y[:,i]))
+    end
+    return diffused
+end
+
+function f(model, diffused)
+    x = diffused.x
+    x_0, _ = model(cat(x, zero(x), dims = 3), diffused.y, diffused.t)
+    x_0 = sigmoid.(x_0)
+    x_0[:,:,:,rand(size(x_0, 4)) .< 0.5] .= 0  # nullify images with 50% probability
+    return (x = cat(x, x_0, dims = 3), y = diffused.y, t = diffused.t)
+end
+
+starttime = now()
+for epoch in 1:n_epochs
+    loss_train = 0.0
+    for (x, y) in dataloader_train
+        diffused = diffuse(x, y)
+        x, y, diffused = device((x, y, diffused))
+        if self_conditioned
+            diffused = f(model, diffused)
+        end
+        loss, grads = Flux.withgradient(model) do model
+            x̂, ŷ = model(diffused.x, diffused.y, diffused.t)
+            reconst = mse(sigmoid.(x̂), x)
+            class = logitcrossentropy(ŷ, y)
+            return reconst + λ * class
+        end
+        Optimisers.update!(state, model, grads[1])
+        loss_train += loss
+    end
+    loss_train /= length(dataloader_train)
+
+    loss_reconst = loss_class = 0.0
+    for (x, y) in dataloader_test
+        diffused = diffuse(x, y)
+        x, y, diffused = device((x, y, diffused))
+        if self_conditioned
+            diffused = f(model, diffused)
+        end
+        x̂, ŷ = model(diffused.x, diffused.y, diffused.t)
+        loss_reconst += mse(sigmoid.(x̂), x)
+        loss_class += logitcrossentropy(ŷ, y)
+    end
+    loss_reconst /= length(dataloader_test)
+    loss_class /= length(dataloader_test)
+    loss_test = loss_reconst + λ * loss_class
+
+    if epoch % 50 == 0
+        let unet = cpu(model), name = @sprintf("%s-%03d.bson", lowercase(string(dataset)), epoch)
+            @info "Saving weights $(name)"
+            @save name unet
+        end
+    end
+
+    elapsed = Dates.value(now() - starttime) / 1000
+    @printf "%d: train=%f test=%f reconst=%f class=%f elapsed=%.1f\n" epoch loss_train loss_test loss_reconst loss_class elapsed
+end
