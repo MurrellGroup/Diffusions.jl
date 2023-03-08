90b96a7cfa08f80d68abd3f746a2caa2693e9ab6
Kenta Sato <bicycle1885@gmail.com>
2023-03-08 17:16:26 +0100

    remove normalization of IndependentDiscreteDiffusion

--- src/discrete.jl
+++ src/discrete.jl
@@ -3,28 +3,25 @@
 struct IndependentDiscreteDiffusion{T <: Real} <: DiscreteStateProcess
     r::T
     π::Vector{T}
-    β::T
+end
 
-    function IndependentDiscreteDiffusion(r::T, π::AbstractVector{<: T}) where T <: Real
-        π = π ./ sum(π)
-        β = inv(1 - sum(abs2, π))
-        return new{T}(r, π, β)
-    end
+function IndependentDiscreteDiffusion(r::Real, π::AbstractVector{<: Real})
+    return IndependentDiscreteDiffusion(float(r), π ./ sum(π))
 end
 
 eq_dist(model::IndependentDiscreteDiffusion) = Categorical(model.π)
 
 function forward(process::IndependentDiscreteDiffusion, x_s::AbstractArray, s::Real, t::Real)
-    (;r, π, β) = process
-    pow = exp(-β * r * (t - s))
+    (;r, π) = process
+    pow = exp(-r * (t - s))
     c1 = (1 - pow) .* π
     c2 = pow .+ c1
     return CategoricalVariables(@. c1 * (1 - x_s) + c2 * x_s)
 end
 
 function backward(process::IndependentDiscreteDiffusion, x_t::AbstractArray, s::Real, t::Real)
-    (;r, π, β) = process
-    pow = exp(-β * r * (t - s))
+    (;r, π) = process
+    pow = exp(-r * (t - s))
     c1 = (1 - pow) .* π
     return pow .* x_t .+ sum(x_t .* c1, dims = 1)
 end
