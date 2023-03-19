46eec75136439bec70a9ecd5f7962b17db53e7e1
aronStalmarck <76658998+aronStalmarck@users.noreply.github.com>
2023-03-19 14:07:43 +0100

    Bugfix

--- src/manifolds.jl
+++ src/manifolds.jl
@@ -2,9 +2,9 @@
 If you want to use a manifold not in Manifolds.jl, you can define a new type that inherits from Manifold and implement the following functions:
 - project(manifold, point)
 - shortest_path_interpolation(rng, process, point_0, point_t, s, t)
-
-# x_t = Array(euclidean_dimension, batch_dims...)
 """
+
+# x_t = Array(euclidean_dimension, batch_size)
 
 struct ManifoldBrownianDiffusion{M <: AbstractManifold, T <: Real} <: SamplingProcess
     manifold::M
@@ -14,17 +14,18 @@ end
 
 ManifoldBrownianDiffusion(manifold::AbstractManifold, rate::T) where T <: Real = ManifoldBrownianDiffusion(manifold, rate, (t) -> [t])
 
-function project!(X, manifold)
-    for ix in CartesianIndices(size(X)[2])
-        X[:, ix] = project(manifold, X[:, ix])
+function project!(x_to, x_from, manifold)
+    for ix in CartesianIndices(size(x_from)[2:end])
+        x_to[:, ix] = project(manifold, x_from[:, ix])
     end
 end
 
 function sampleforward(rng::AbstractRNG, process::ManifoldBrownianDiffusion{M, T}, t::Real, x_0) where M where T 
-    x_t = project!(x_0, process.manifold)
+    x_t = similar(x_0)
+    project!(x_t, x_0, process.manifold)
     for step in process.getsteps(t * process.rate)
         x_t .+= randn(rng, T, size(x_t)...) .* sqrt(step)
-        project!(x_t, process.manifold)
+        project!(x_t, x_t, process.manifold)
     end
     return x_t
 end
@@ -35,14 +36,14 @@ shortestpath_interpolation(rng::AbstractRNG, process::ManifoldBrownianDiffusion,
 shortestpath_interpolation_all(rng::AbstractRNG, process::ManifoldBrownianDiffusion, x_0, x_t, s, t) =
     mapslices(indicesofpoint -> shortestpath_interpolation(rng, process, x_0[indicesofpoint], x_t[indicesofpoint], s, t), CartesianIndices(x_0), dims = [1])
 
-# Empirically, this is good - should be the same as diffusing C from B as is done in the rotational/angular cases (nice symmetries) for spheres at least.
+# Empirically, this is really good, but I am not completely sure why.
 function endpoint_conditioned_sample(rng::AbstractRNG, process::ManifoldBrownianDiffusion, s::Real, t::Real, x_0, x_t)
     B = sampleforward(rng, process, s, x_0)
     C = sampleforward(rng, process, t-s, x_t)
     return shortestpath_interpolation_all(rng, process, B, C, s, t)
 end
 
-# This is closer to the bridge trick for the angular diffusion but the code is not as nice, and empirically it is not as good - but should give the same result, will redo tests. 
+# This does the same as for the angular diffusion but the code is not as nice, and empirically it is not as good.
 #=
 function endpoint_conditioned_sample(rng::AbstractRNG, process::ManifoldBrownianDiffusion, s::Real, t::Real, x_0, x_t)
     B = sampleforward(rng, process, s, x_0)
