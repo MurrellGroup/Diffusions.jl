437b49aba8bdf05c7ba4df0e970bfafe0caf0485
aronStalmarck <76658998+aronStalmarck@users.noreply.github.com>
2023-03-22 12:22:03 +0100

    Update manifolds.jl

--- src/manifolds.jl
+++ src/manifolds.jl
@@ -2,9 +2,9 @@
 If you want to use a manifold not in Manifolds.jl, you can define a new type that inherits from Manifold and implement the following functions:
 - project(manifold, point)
 - shortest_path_interpolation(rng, process, point_0, point_t, s, t)
-"""
 
-# x_t = Array(euclidean_dimension, batch_size)
+x_t = Array(euclidean_dimension, batch_dims...)
+"""
 
 struct ManifoldBrownianDiffusion{M <: AbstractManifold, T <: Real} <: SamplingProcess
     manifold::M
@@ -12,11 +12,13 @@ struct ManifoldBrownianDiffusion{M <: AbstractManifold, T <: Real} <: SamplingPr
     getsteps::Function
 end
 
-ManifoldBrownianDiffusion(manifold::AbstractManifold, rate::T) where T <: Real = ManifoldBrownianDiffusion(manifold, rate, (t) -> [t])
+ManifoldBrownianDiffusion(manifold::AbstractManifold, rate::T) where T <: Real = ManifoldBrownianDiffusion(manifold, rate, (t) -> range(min(t, 0.05), t, step=0.05))
+
+pointindices(X) = CartesianIndices(size(X)[2:end])
 
 function project!(x_to, x_from, manifold)
-    for ix in CartesianIndices(size(x_from)[2:end])
-        x_to[:, ix] = project(manifold, x_from[:, ix])
+    for pointindex in pointindices(x_from)
+        x_to[:, pointindex] = project(manifold, x_from[:, pointindex])
     end
 end
 
@@ -33,21 +35,42 @@ end
 shortestpath_interpolation(rng::AbstractRNG, process::ManifoldBrownianDiffusion, p_0::AbstractVector, p_t::AbstractVector, s, t) = 
     shortest_geodesic(process.manifold, p_0, p_t, s / t)
 
-shortestpath_interpolation_all(rng::AbstractRNG, process::ManifoldBrownianDiffusion, x_0, x_t, s, t) =
-    mapslices(indicesofpoint -> shortestpath_interpolation(rng, process, x_0[indicesofpoint], x_t[indicesofpoint], s, t), CartesianIndices(x_0), dims = [1])
+function shortestpath_interpolation!(x_s, rng::AbstractRNG, process::ManifoldBrownianDiffusion, x_0, x_t, s, t)
+    for pointindex in pointindices(x_s)
+        x_s[:, pointindex] = shortestpath_interpolation(rng, process, x_0[:, pointindex], x_t[:, pointindex], s, t)
+    end
+end
 
-# Empirically, this is really good, but I am not completely sure why.
+# Empirically, this is good - should be the same as diffusing C from B as is done in the rotational/angular cases (nice symmetries) for spheres at least.
 function endpoint_conditioned_sample(rng::AbstractRNG, process::ManifoldBrownianDiffusion, s::Real, t::Real, x_0, x_t)
     B = sampleforward(rng, process, s, x_0)
     C = sampleforward(rng, process, t-s, x_t)
-    return shortestpath_interpolation_all(rng, process, B, C, s, t)
+    shortestpath_interpolation!(C, rng, process, B, C, s, t)
+    return C
 end
 
-# This does the same as for the angular diffusion but the code is not as nice, and empirically it is not as good.
-#=
-function endpoint_conditioned_sample(rng::AbstractRNG, process::ManifoldBrownianDiffusion, s::Real, t::Real, x_0, x_t)
-    B = sampleforward(rng, process, s, x_0)
-    C = sampleforward(rng, process, t-s, B)
-    return [exp(process.manifold, b, log(process.manifold, c, shortest_geodesic(process.manifold, c, endpoint, (t-s) / t))) for (b, c, endpoint) in zip(B, C, x_t)]
+# This has not been tested yet. An optional way of a heuristic brownian bridge for general manifolds.
+function endpoint_conditioned_sample_distance_proportional(rng::AbstractRNG, process::ManifoldBrownianDiffusion, s::Real, t::Real, x_0, x_t)
+    x_s = similar(x_0)
+    D = process.rate # Diffusion coefficient
+    dt = min(s, 0.05) # timestep
+    for pointindex in pointindices(x_from)
+        p_0 = x_0[:, pointindex]
+        p_t = x_t[:, pointindex]
+        d = distance(process.manifold, p_0, p_t)
+        t_remaining = t
+        p_cur = p_0
+        while (t - t_remaining) < s
+            dp = randn(rng, size(p_cur)...) .* (2 * D * dt)
+            p_new = p_cur .+ dp
+            d_remaining = distance(process.manifold, p_new, p_t)
+            factor = 1 - (d_remaining / d) * (t_remaining / T)
+            p_adjusted = shortest_geodesic(p_new, p_t, factor)
+
+            p_cur = project(process.manifold, p_adjusted)
+            t_remaining -= dt
+        end
+        x_s[:, pointindex] = p_cur
+    end
+    x_s
 end
-=#
